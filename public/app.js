31faa12825ed522bd716c7e188ef197747edae11
karol-szykula <karol.szykula@wp.pl>
2018-12-15 19:30:18 +0100

    Add login page

--- public/app.js
+++ public/app.js
@@ -153,3 +153,89 @@ const showChat = async () => {
 
     users.data.forEach(addUser);
 };
+
+
+// Retrieve email/password object from the login/signup page
+const getCredentials = () => {
+    const user = {
+        email: document.querySelector('[name="email"]').value,
+        password: document.querySelector('[name="password"]').value
+    };
+
+    return user;
+};
+
+// Log in either using the given email/password or the token from storage
+const login = async credentials => {
+    try {
+        if (!credentials) {
+            // Try to authenticate using the JWT from localStorage
+            await client.authenticate();
+        } else {
+            // If we get login information, add the strategy we want to use for login
+            const payload = Object.assign({ strategy: 'local' }, credentials);
+
+            await client.authenticate(payload);
+        }
+
+        // If successful, show the chat page
+        showChat();
+    } catch (error) {
+        // If we got an error, show the login page
+        showLogin(error);
+    }
+};
+
+document.addEventListener('click', async ev => {
+    switch (ev.target.id) {
+        case 'signup': {
+            // For signup, create a new user and then log them in
+            const credentials = getCredentials();
+
+            // First create the user
+            await client.service('users').create(credentials);
+            // If successful log them in
+            await login(credentials);
+
+            break;
+        }
+        case 'login': {
+            const user = getCredentials();
+
+            await login(user);
+
+            break;
+        }
+        case 'logout': {
+            await client.logout();
+
+            document.getElementById('app').innerHTML = loginHTML;
+
+            break;
+        }
+    }
+});
+
+document.addEventListener('submit', async ev => {
+    if (ev.target.id === 'send-message') {
+        // This is the message text input field
+        const input = document.querySelector('[name="text"]');
+
+        ev.preventDefault();
+
+        // Create a new message and then clear the input field
+        await client.service('messages').create({
+            text: input.value
+        });
+
+        input.value = '';
+    }
+});
+
+// Listen to created events and add the new message in real-time
+client.service('messages').on('created', addMessage);
+
+// We will also see when new users get created in real-time
+client.service('users').on('created', addUser);
+
+login();
